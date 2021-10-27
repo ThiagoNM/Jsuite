79627d000630eb56d86f8e4d208a0ab5bbef7b60
Thiago <thnomo@fp.insjoaquimmir.cat>
2021-10-27 17:13:48 +0200

    Carpeta config

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+ 
+return [
+   "driver"    => "mysql",
+   "host"      => "127.0.0.1",
+   "port"      => 3306,
+   "database"  => "testdb",
+   "user"      => "dbuser",
+   "password"  => "dbpass"   
+];
+
