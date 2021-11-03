b5db43f3635f5181ed2b25a76cc9aa2ad87d145c
Thiago <thnomo@fp.insjoaquimmir.cat>
2021-11-03 17:30:28 +0100

    mail dades de exemple

---
+++ config/mail.php
@@ -0,0 +1,21 @@
+<?php
+return [
+   "server" => [
+       "protocol"  => "smtp",
+       "security"  => "tls",
+       "host"      => "smtp.gmail.com",
+       "port"      => 587,
+       "username"  => "your-email@gmail.com",
+       "password"  => "your-gmail-password",
+       "debug"     => 1
+   ],
+   "from" => [
+       "name"      => "your-name",
+       "mail"      => "your-email@gmail.com"
+   ],
+   "reply" => [
+       "name"      => "your-name",
+       "mail"      => "your-email@gmail.com"
+   ]
+];
+
