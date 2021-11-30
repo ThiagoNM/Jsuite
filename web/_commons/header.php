90d1162a078ecf6a3e66852a77cfd5a6c1c74672
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2021-11-30 18:31:13 +0100

    archivos master

--- web/_commons/header.php
+++ web/_commons/header.php
@@ -1,5 +1,13 @@
-<header>
-<h1><a href="<?= My\Helpers::url("/") ?>">Projecte J-Suite</a></h1>
+<header class="capçalera">
+     <div class = "container4">
+          <img class="capçalera__logo" src="<?= My\Helpers::url("/_commons/fotos/JS6.png")?>"/>
+          <img class="capçalera__fotoPerfil" src="<?= My\Helpers::url("/_commons/fotos/exempleperfil.jpg")?>"/>
+          <img class="capçalera__tema" src="<?= My\Helpers::url("/_commons/fotos/dark.png")?>"/>
+          <img class="capçalera__missatgeria" src="<?= My\Helpers::url("/_commons/fotos/missatge.png")?>"/>
+          <h2 class="capçalera__nom">Nom Usuari Lorem</h2>
+     </div>
+</header>
+
 <?php $flash = My\Helpers::flash(); ?>
 <?php if (!empty($flash)): ?>
 <div class="flash">
@@ -10,4 +18,12 @@
      </ul>
 </div>
 <?php endif; ?>
-</header>
+
+
+<!--<div class="flash">
+<?php if (!empty($flash)): ?>     <ul>
+          <?php foreach ($flash as $msg): ?>
+          <li class="flash__message"><?= $msg ?></li>
+          <?php endforeach; ?>
+     </ul>
+<?php endif; ?></div>-->
