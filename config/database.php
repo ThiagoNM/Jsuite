4f10f5f974947b06a465d182f010f09834834da6
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2021-11-30 20:00:58 +0100

    forgot4

--- config/database.php
+++ config/database.php
@@ -4,14 +4,15 @@ return [
    "driver"    => "mysql",
    "host"      => "alumnes.insjoaquimmir.cat",
    "port"      => 9316,
-   "database"  => "demo",
-   "user"      => "demo",
-   "password"  => "VGz739ghGmQU",
+   "database"  => "2daw.equip03",
+   "user"      => "2daw.equip03",
+   "password"  => "L8Ry4=<U",
    "options"   => [
        PDO::MYSQL_ATTR_SSL_KEY                => __DIR__ . '/client-key.pem',
        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
    ]
 ];
+
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password);
