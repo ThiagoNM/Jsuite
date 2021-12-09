2af975d0f7a97110462c335f33113867cd702167
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2021-12-09 16:27:21 +0100

    canvis

--- config/database.php
+++ config/database.php
@@ -2,9 +2,21 @@
  
 return [
    "driver"    => "mysql",
-   "host"      => "127.0.0.1",
-   "port"      => 3306,
-   "database"  => "demo",
-   "user"      => "demo",
-   "password"  => "dbpass"
-?>
+   "host"      => "alumnes.insjoaquimmir.cat",
+   "port"      => 9316,
+   "database"  => "2daw.equip03",
+   "user"      => "2daw.equip03",
+   "password"  => "L8Ry4=<U",
+   "options"   => [
+       PDO::MYSQL_ATTR_SSL_KEY                => __DIR__ . '/client-key.pem',
+       PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+   ]
+];
+
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+
+} catch (PDOException $exception) {
+    echo "A fallat la conexion a la base: {$exception->getMessage()}";
+}
