358f6f078e7e6fc1ca0ea6a4d444b3b0bcf751d3
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2022-01-12 17:56:40 +0100

    Microframework folder

--- microframework/library/Helpers.php
+++ microframework/library/Helpers.php
@@ -37,7 +37,7 @@ class Helpers {
    public static function url(string $path, bool $ssl = false): string 
    {
       $protocol = $ssl ? "https" : "http";
-      return "{$protocol}://localhost/tarda/Jsuite/web{$path}";
+      return "{$protocol}://localhost/tarda/Jsuite/microframework/web{$path}";
    }
    public static function render(string $path, array $__params = []) : string 
    {
