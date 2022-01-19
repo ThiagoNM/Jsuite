d2316315ddc29aca95df8244b7fb84d2a436b10e
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2022-01-19 18:50:51 +0100

    laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
-
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
--- laravel/storage/framework/views/8399b71f23056fd0280a00f91a719559513f9687.php
+++ laravel/storage/framework/views/8399b71f23056fd0280a00f91a719559513f9687.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -21,6 +22,7 @@
         </style>
     </head>
     <body class="antialiased">
+        <?php echo $__env->make('flash', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <?php if(Route::has('login')): ?>
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
