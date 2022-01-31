4d0cc0f6c9b830f7e229f36dc265b3b5c1a7b2c7
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2022-01-31 16:05:28 +0100

    afegit1

--- design/js/config.js
+++
@@ -1,9 +0,0 @@
-function PlaySound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.play();
-}
-
-function StopSound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.pause();
-}
