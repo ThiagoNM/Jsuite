2f93333a3ecc631855db0b5b4e1f8a9877e8b91a
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2022-01-31 16:04:37 +0100

    esborrat

--- microframework/scripts/hello.php
+++
@@ -1,7 +0,0 @@
-<?php
- 
-// Load composer dependencies
-require_once __DIR__ . "/../vendor/autoload.php";
- 
-$username = get_current_user();
-echo My\Helpers::sayHello($username) . PHP_EOL;
--- microframework/web/_commons/head.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projecte J-Suite / <?= $__params["subtitle"] ?? "" ?></title>
-    <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/reset.css")?>">
-    <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/layout.css")?>">
-    <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/styles.css")?>">
-    <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/components.css")?>">
-</head>
