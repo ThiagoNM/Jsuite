6e5d643e2e8e111bba3dc640caa6918262c05080
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2022-02-11 18:46:13 +0100

    mapa

---
+++ design/js/config.js
@@ -0,0 +1,12 @@
+/* pk.eyJ1IjoidmFsZW50aXBwIiwiYSI6ImNremg0cjdwbTJucHgybm8xazR6enQ3ZXQifQ._BJ1QyBTRntc61LN6G_Kfw */
+
+mapboxgl.accessToken='pk.eyJ1IjoidmFsZW50aXBwIiwiYSI6ImNremg0cjdwbTJucHgybm8xazR6enQ3ZXQifQ._BJ1QyBTRntc61LN6G_Kfw'
+
+let map = new mapboxgl.Map({
+
+    container:'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center:[1.728508, 41.231164],
+    zoom: 18
+
+})
