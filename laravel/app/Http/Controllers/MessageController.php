7e22ea728bbb4b30083e2b127912dbecefe665a1
Valenti <vpozzanp@fp.insjoaquimmir.cat>
2022-02-15 18:30:37 +0100

    messages

---
+++ laravel/app/Http/Controllers/MessageController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $messages = Message::all();
+        return \response($messages);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'message' => 'required|max:255',
+            'chat_id' => 'required',
+            'author_id' => 'required'
+
+        ]);
+
+        $message = Message::create($request->all());
+        return \response($message);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $message = Message::findOrFail($id);
+        return \response($message);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Message::findOrFail($id)
+            ->update($request->all());
+        return \response("Mensaje actualizado.");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Message::destroy($id);
+        return \response("El mensaje con el id: ${id} ha sido eliminado.");
+    }
+}
