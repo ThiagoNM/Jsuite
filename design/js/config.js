cb4ab5a4e2122876c02c2f809c5cc17a3c151144
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-02-23 19:05:02 +0100

    Jsuite

--- design/js/config.js
+++ design/js/config.js
@@ -1,19 +1,3 @@
-// <!---------- MAPBOX ---------->
-
-// /* pk.eyJ1IjoidmFsZW50aXBwIiwiYSI6ImNremg0cjdwbTJucHgybm8xazR6enQ3ZXQifQ._BJ1QyBTRntc61LN6G_Kfw */
-
-// mapboxgl.accessToken='pk.eyJ1IjoidmFsZW50aXBwIiwiYSI6ImNremg0cjdwbTJucHgybm8xazR6enQ3ZXQifQ._BJ1QyBTRntc61LN6G_Kfw'
-
-// let map = new mapboxgl.Map({
-
-//     container:'map',
-//     style: 'mapbox://styles/mapbox/streets-v10',
-//     center:[1.728508, 41.231164],
-//     zoom: 18
-
-// })
-
-
 // MAPA
 var map = L.map('mapa').setView([41.231322284217, 1.7281074555046259], 18);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -34,16 +18,10 @@ if (navigator.geolocation) {
     alert("No hemos podido coger tu ubicación.");
 }
 
-
 // 2 MARCADOR
 function mostrarPosicio(position) {
-    
     var marker2 = L.circle([position.coords.latitude, position.coords.longitude], {radius: 30, color: 'red'}).bindPopup('ESTÀS PER AQUESTA ZONA').addTo(map);
     marker2.openPopup();
-
-    Mousetrap.bind('ctrl+shift+k', function(e) {
-        alert(position.coords.latitude + " " + position.coords.longitude)
-    });
 
     // RUTA
     L.Routing.control({
@@ -51,6 +29,4 @@ function mostrarPosicio(position) {
         L.latLng(41.23118511665772, 1.7280967266692684),
         L.latLng(position.coords.latitude, position.coords.longitude)
     ]}).addTo(map);
-
-
 }
