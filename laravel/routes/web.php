a3825fd13c43b0ef08b3e2a15f0b9dfd6caa9b0e
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-03-11 18:42:10 +0100

    bootstrap laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -30,3 +30,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
