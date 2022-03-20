f8bbf8abcb5a6f15afdd5cfc35e348f67695873a
vapopi <91739022+vapopi@users.noreply.github.com>
2022-03-20 18:13:12 +0100

    Add files via upload

---
+++ design/js/configg.js
@@ -0,0 +1,9 @@
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+}
