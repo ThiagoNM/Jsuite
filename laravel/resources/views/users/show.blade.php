10b5183e67596bb420b4f3b5355890a6b1f7c3f5
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-04-21 19:21:24 +0200

    crud

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -25,25 +25,56 @@
         </div>
 
     </div>
-
     <ul class="list-group list-group-flush">
         <li class="list-group-item">
             <a href="{{ route('users.edit', $user->id) }}" class="w-100 btn btn-warning" role="button">Editar</a>
         </li>
 
         <li class="list-group-item">
-            <form method="post" action="{{ route('users.destroy', $user->id) }}">
+        <form id="form" method="POST" action="{{ route('users.destroy', $user) }}">
                 @csrf
                 @method("DELETE")
-                <button class="w-100 btn btn-danger" type="submit" onClick="document.getElementById('spin').style.display='inline-block'">
-                    <div id="spin" style="display: none;" class="spinner-border spinner-border-sm" role="status"></div>
-                    Esborrar
-                </button>
+                <button id="destroy" type="submit" class="w-100 btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmModal">Esborrar</button>
             </form>
+            <!-- Modal -->
+            <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Estas segur?</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Aquesta acció no es pot desfer
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel·lar</button>
+                            <button id="confirm" class="btn btn-primary" type="submit" onClick="document.getElementById('spin').style.display='inline-block'">
+                                <div id="spin" style="display: none;" class="spinner-border spinner-border-sm" role="status"></div>
+                                Confirmar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </li>
     </ul>
-
   </div>
 </div>
+
+
+<script type="text/javascript">                
+    const submit = document.getElementById('destroy')
+    const  confirm = document.getElementById('confirm')
+
+    submit.addEventListener("click", function( event ) {
+        event.preventDefault()
+        return false
+    })
+
+    confirm.addEventListener("click", function( event ) {
+        document.getElementById("form").submit(); 
+    })
+</script>
 
 @endsection
